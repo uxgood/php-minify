74967d21cd749aaa85c0b0f7edc2265f9bca239b
UxGood <uxgood.org@gmail.com>
2018-12-21 15:56:45 +0800

    fix errors in demo2.php

--- demo2.php
+++ demo2.php
@@ -1,15 +1,34 @@
 <?php
-$x;
-${x};
-${'x'};
-${"x"};
-$$x;
-"$x";
-"${x}";
-"${'x'}";
-"{$x}";
-"{$x[0]}";
-"${x[0]}";
-"{$x->x}";
-"{${x}->x}";
-"{${'x'}->x}";
+namespace a\b\c
+{
+    define('x', 'x');
+    function a () {
+        $x =1;
+        $y = 'x';
+        echo $x;
+        echo ${x};
+        echo ${'x'};
+        echo ${"x"};
+        echo $$y;
+        echo "$x";
+        echo "${x}";
+        echo "${'x'}";
+        echo "{$x}";
+        $x=array(2);
+        echo "{$x[0]}";
+        echo "${x[0]}";
+        $x = new \StdClass();
+        $x->x = 3;
+        echo "{$x->x}";
+        echo "{${x}->x}";
+        echo "{${'x'}->x}";
+    }
+    a();
+}
+namespace a\b\d
+{
+    class a
+    {
+    
+    }
+}
